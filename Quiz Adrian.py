a5b9b605977ae6b9b67071f4a9b0e7d28cafc9c7
SkyHunter2099 <47864220+SkyHunter2099@users.noreply.github.com>
2019-02-21 11:16:18 -0600

    Add files via upload
    Programa de Producto y Promedio

---
+++ Quiz Adrian.py
@@ -0,0 +1,11 @@
+
+def quiz(a,b,c):
+
+    Suma=(a+b)
+    if (Suma>c):
+        Producto=(a*b*c)
+        Promedio=(a+b+c)/3
+        print("El producto de los 3 numeros es:",Producto)
+        print("El promedio de los 3 numeros es:",Promedio)
+    else:
+        print("No se puede realizar la operacion")
